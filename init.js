559db46bf8355a19f46503b06dbcdeb7c8a7b3c2
SuperShrew <joshua.gerrish@outlook.com>
2026-09-25 22:29:32 +0100

    theme changing code (50% done)

--- init.js
+++ init.js
@@ -2,12 +2,21 @@ const gameArea = document.getElementById("game");
 const menuBox = document.getElementById("menu");
 menuBox.innerHTML += "<h1> test </h1>";
 
-function colours(theme) {
-    if (theme == null) {
+function colours(colour1, colour2) { // add colour 3 4 and 5 for button text and body colours
+    if (colour1 == null && colour2 == null) {
         menuBox.innerHTML = `
-            <button onclick="colours(1)">click1</button>
+            Colour 1: <input type="text" name="c1" value = "#00ff00"><br>
+            Colour 2: <input type="text" name="c2" value = "#0000ff"><br><br>
+            <button onclick="colours(document.getElementsByName('c1'), document.getElementsByName('c2'))">Confirm Colours</button>
+            <button onclick="switchText()">Flip text colour</button>
         `;
-    } else if (theme == 1) {
-        menuBox.innerHTML = "test2";
+    } else if (colour1[0] && colour2[0]) {
+        console.log("wawo");
+        let elements = document.querySelectorAll(".window");
+        elements.forEach(element => {
+            element.style.backgroundColor = colour1[0].value;
+            console.log("sigh");
+            element.style.borderColor = colour2[0].value;
+        });
     }
 }
