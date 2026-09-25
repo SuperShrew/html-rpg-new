c5bb2f45b3bbdc9cbd60ffd4f71ebc9b8a3f5339
Clover Gerrish <150274640+SuperShrew@users.noreply.github.com>
2026-09-25 13:47:24 +0100

    temporary tests for themes

--- init.js
+++ init.js
@@ -1,2 +1,13 @@
 const gameArea = document.getElementById("game");
-gameArea.text = "test";
+const menuBox = document.getElementById("menu");
+menuBox.innerHTML += "<h1> test </h1>";
+
+function colours(theme) {
+    if (theme == null) {
+        menuBox.innerHTML = `
+            <button onclick="colours(1)">click1</button>
+        `;
+    } else if (theme == 1) {
+        menuBox.innerHTML = "test2";
+    }
+}
